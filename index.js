1f30c118792f5978b2e002a810e517034d6e9e07
adithya-naik <jatothadithyanaik@gmail.com>
2025-03-01 01:56:57 +0530

    fixed cors

--- index.js
+++ index.js
@@ -1,30 +1,65 @@
-const express = require('express')
+// const express = require('express')
+// const mongoDB = require('./db');
+// const app = express()
+// require('dotenv').config()
+// mongoDB();
+
+
+// const port = process.env.PORT
+
+// app.use((req, res, next) => {
+//   // react app port number 
+//   res.setHeader('Access-Control-Allow-Origin', 'https://myifoodb.onrender.com');
+//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type,auth-token, Accept');
+//   next();
+// });
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World! ----')
+// })
+
+
+// app.use(express.json());
+// app.use('/api', require('./routes/createUser'));
+// app.use('/api', require('./routes/getData'));
+// app.use('/api', require('./routes/orderData'));
+// app.use('/api', require('./routes/fetchUserOrders'));
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
+
+
+
+
+
+const express = require('express');
 const mongoDB = require('./db');
-const app = express()
-require('dotenv').config()
+const cors = require('cors');
+const app = express();
+require('dotenv').config();
 mongoDB();
 
+const port = process.env.PORT;
 
-const port = process.env.PORT
-
-app.use((req, res, next) => {
-  // react app port number 
-  res.setHeader('Access-Control-Allow-Origin', 'https://myifoodb.onrender.com');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type,auth-token, Accept');
-  next();
-});
-
-app.get('/', (req, res) => {
-  res.send('Hello World! ----')
-})
-
+// Configure cors middleware to allow all origins
+app.use(cors({
+  origin: '*', // Allow all origins
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'auth-token', 'Accept']
+}));
 
 app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World! ----');
+});
+
 app.use('/api', require('./routes/createUser'));
 app.use('/api', require('./routes/getData'));
 app.use('/api', require('./routes/orderData'));
 app.use('/api', require('./routes/fetchUserOrders'));
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
