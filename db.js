4a92aa5456fb672bc1021c51fb54bbd5043984d4
adithya-naik <jatothadithyanaik@gmail.com>
2025-03-01 19:09:21 +0530

    fixed warnings

--- db.js
+++ db.js
@@ -12,10 +12,12 @@ global.food_category = [];
 
 const mongoDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
+    // await mongoose.connect(process.env.MONGO_URI, {
+    //   useNewUrlParser: true,
+    //   useUnifiedTopology: true
+    // });
+
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB connected successfully");
 
     const db = mongoose.connection.db;
